d051b7e9407e9db63b65dfe9b2a8fed610af72c2
Wilson Leung <wilsonpsleung@gmail.com>
2021-05-21 20:14:37 +0800

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,53 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+const API_URL = 'https://reqres.in/api/users';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  showLoading = false;
+
   title = 'angular-change-detection';
+
+  constructor(private httpClient: HttpClient) {}
+
+  startLoading(): void {
+    console.log('[startLoading] ...');
+    this.showLoading = true;
+  }
+
+  stopLoading(): void {
+    console.log('[stopLoading] ...');
+    this.showLoading = false;
+  }
+
+  startTimeout() {
+    setTimeout(() => {
+      this.showLoading = true;
+    }, 50);
+  }
+
+  stopTimeout() {
+    setTimeout(() => {
+      this.showLoading = false;
+    }, 50);
+  }
+
+  httpStart() {
+    this.httpClient.get(API_URL).subscribe((response) => {
+      console.log('[api response] ...', response);
+      this.showLoading = true;
+    });
+  }
+
+  httpStop() {
+    this.httpClient.get(API_URL).subscribe((response) => {
+      console.log('[api response] ...', response);
+      this.showLoading = false;
+    });
+  }
 }
